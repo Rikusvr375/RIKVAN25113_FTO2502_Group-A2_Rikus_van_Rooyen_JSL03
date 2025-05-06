7cff0122ccaf77bdb56d51b09acf230cbd8f48ea
Rikus van Rooyen <rikusvr12@gmail.com>
2025-05-06 18:52:33 +0200

    wrote code for incremented ID's, next up, prompts.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,43 @@
-const initialTasks = [
-  {
-    id: 1,
-    title: "Launch Epic Career",
-    description: "Create a killer Resume",
-    status: "todo",
-  },
-  {
-    id: 2,
-    title: "Master JavaScript",
-    description: "Get comfortable with the fundamentals",
-    status: "doing",
-  },
-  {
-    id: 3,
-    title: "Contribute to Open Source Projects",
-    description:
-      "Gain practical experience and collaborate with others in the software development community",
-    status: "done",
-  },
-];
+  let task = [];
+
+// Initial tasks
+   task.push(
+    {
+      id: 1,
+      title: "Launch Epic Career",
+      description: "Create a killer Resume",
+      status: "todo",
+    });
+   task.push({
+      id: 2,
+      title: "Master JavaScript",
+      description: "Get comfortable with the fundamentals",
+      status: "doing",
+    });
+   task.push({
+      id: 3,
+      title: "Contribute to Open Source Projects",
+      description:
+        "Gain practical experience and collaborate with others in the software development community",
+      status: "done",
+    });
+
+
+// Code for incremented id's
+  
+let updatedId = task[task.length-1].id+1;
+
+// New Tasks
+
+for (let i = 0; i < 3; i++) {
+
+  task.push({
+      id: updatedId,
+      title: "aiub",
+      description:"iasuf",
+      status: "asojf",
+    });
+    updatedId++
+  }
+  
 
